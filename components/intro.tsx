ec09250bbc72cb606310656ef82c2941206a4ebe
Partho Ghosh <partho.ghosh24@gmail.com>
2024-04-17 23:58:26 -0600

    Updated image

--- components/intro.tsx
+++ components/intro.tsx
@@ -9,6 +9,7 @@ import { HiDownload } from "react-icons/hi";
 import { FaGithubSquare } from "react-icons/fa";
 import { useSectionInView } from "@/lib/hooks";
 import { useActiveSectionContext } from "@/context/active-section-context";
+import parthoImg from "@/public/partho.jpeg";
 
 export default function Intro() {
   const { ref } = useSectionInView("Home", 0.5);
@@ -31,7 +32,7 @@ export default function Intro() {
             }}
           >
             <Image
-              src="/partho.jpeg"
+              src={parthoImg}
               alt="Partho Ghosh"
               width="192"
               height="192"
